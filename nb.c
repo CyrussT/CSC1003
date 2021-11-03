8099fd126dc1483ad246b84b5446f3f77c804ae0
CyrussT <24277152+CyrussT@users.noreply.github.com>
2021-11-03 23:07:23 +0800

    gaussian functions, update nbstructs data type

--- nb.c
+++ nb.c
@@ -5,11 +5,40 @@
 #include "nbstructs.h"
 
 #define trainingsize 80 /* 80% of data used for training accuracy */
-#define datacount 20   /* 20% of data used for testing accuracy */
+#define datacount 20 /* 20% of data used for testing accuracy */
 #define featuresize 9
 #define PI 3.142
+
+double mean(double arr[], int n);
+double variance(double arr[], int n);
 
 int main()
 {
     return 0;
+}
+
+double mean(double arr[], int n) //needs to run and check output
+{
+    double sum = 0; // sum of all elements
+    for (int i=0; i<n; i++)
+    {
+        sum += arr[i];
+    }
+    return sum/n; // return mean
+}
+
+double variance(double arr[], int n) // needs to run and check output
+{
+    double arr_mean = mean(arr, n);
+    double sum = 0;
+    for (int i=0; i<n; i++)
+    {
+        sum += pow(arr[i] - arr_mean, 2);
+    }
+    return sum/n;
+}
+
+double gaussian(double x, double mean, double variance) //needs to run and check output
+{
+    return (1/(sqrt(2*PI*variance)))*exp(-0.5 * pow(x-mean, 2)/(2*variance));
 }
--- nbstructs.h
+++ nbstructs.h
@@ -1,13 +1,13 @@
-typedef struct 
+typedef struct features 
 {
     float season; // Season of analysis: -1 for Winter, -0.33 for Spring, 0.33 for Summer, 1 for Fall
     double age; // Age of analysis: Age 18 to 36: 0 to 1
     unsigned char disease; // Childish Disease: 0 for yes, 1 for no
     unsigned char accident; // Accident or serious trauma: 0 for yes, 1 for no
     unsigned char surgery; // Surgery intervention: 0 for yes, 1 for no
-    int fever; // High fever in last year: -1 for less than three months ago, 0 for more than three months ago, 1 for no.
+    char fever; // High fever in last year: -1 for less than three months ago, 0 for more than three months ago, 1 for no.
     float alcohol; // Frequency of alcohol consumption: 0.2 for several times a day, 0.4 for everyday, 0.6 for several times a week, 0.8 for once a week, 1 for hardly ever or ever.
-    int smoker; // Smoking Habit: -1 for never, 0 for occassional, 1 for daily
+    char smoker; // Smoking Habit: -1 for never, 0 for occassional, 1 for daily
     double sittinghours; // Hours spent sitting down per day: 0 to 1 (0 for 0 hours, 1 for 24 hours?) 
     char semendiagnosis; // Semen Diagnosis: 0 for Normal, 1 for Altered
 } features;
