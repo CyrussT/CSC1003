b0e73b830b9a4141c275c060551d9b844a24821a
CyrussT <24277152+CyrussT@users.noreply.github.com>
2021-11-03 21:24:23 +0800

    init nb.c

---
+++ nb.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define trainingsize 80 /* 80% of data used for training accuracy */
+#define datacount 20   /* 20% of data used for testing accuracy */
+#define featuresize 9
+#define PI 3.142
+
+int main()
+{
+    return 0;
+}
